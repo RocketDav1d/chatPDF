ac7ca5f42cf168a5b4ee8b5bbbb88ff06e074eab
David Korn <me@davidkorn.de>
2023-10-09 23:00:48 +0200

    add subscription & finish Chat

--- src/app/chat/[chatId]/page.tsx
+++ src/app/chat/[chatId]/page.tsx
@@ -1,45 +1,104 @@
+// import { db } from "@/lib/db";
+// import { chats } from "@/lib/db/schema";
+// import { auth } from "@clerk/nextjs";
+// import { eq } from "drizzle-orm";
+// import { redirect } from "next/navigation";
+// import React from "react";
+
+// type Props = {
+//     params: {
+//         chatId: string
+//     }
+// }
+
+
+// const Chat = async ({params: {chatId}}: Props) => {
+//     const {userId} = await auth()
+//     if (!userId) {
+//         return redirect('/sign-in')
+//     }
+//     const _chats = await db.select().from(chats).where(eq(chats.userId, userId))
+//     if (!_chats) {
+//         return redirect('/')
+//     }
+//     // if (!_chats.find(chat => chat.id === parseInt(chatId))) {
+//     //     return redirect('/')
+//     // }
+//     return (
+//         <div className="flex max-h-screen overflow-scroll ">
+//             <div className="flex w-full max-h-screen overflow-scroll">
+//                 <div className="flex-[1] max-w-xs">
+//                     <ChatSidebar />
+//                 </div>
+//                 <div className="max-h-screen p-4 overflow-scroll flex-[5]">
+
+//                 </div>
+//                 <div className="flex-[3] border-l-4 border-l-slate-200">
+
+//                 </div>
+
+//             </div>
+//         </div>
+//     )
+// }
+
+// export default Chat
+
+
+
+
+
+import ChatComponent from "@/components/ChatComponent";
+import ChatSideBar from "@/components/ChatSideBar";
+import PDFViewer from "@/components/PDFViewer";
 import { db } from "@/lib/db";
 import { chats } from "@/lib/db/schema";
+import { checkSubscription } from "@/lib/subscription";
 import { auth } from "@clerk/nextjs";
 import { eq } from "drizzle-orm";
 import { redirect } from "next/navigation";
 import React from "react";
 
 type Props = {
-    params: {
-        chatId: string
-    }
-}
+  params: {
+    chatId: string;
+  };
+};
 
+const ChatPage = async ({ params: { chatId } }: Props) => {
+  const { userId } = await auth();
+  if (!userId) {
+    return redirect("/sign-in");
+  }
+  const _chats = await db.select().from(chats).where(eq(chats.userId, userId));
+  if (!_chats) {
+    return redirect("/");
+  }
+  if (!_chats.find((chat) => chat.id === parseInt(chatId))) {
+    return redirect("/");
+  }
 
-const Chat = async ({params: {chatId}}: Props) => {
-    const {userId} = await auth()
-    if (!userId) {
-        return redirect('/sign-in')
-    }
-    const _chats = await db.select().from(chats).where(eq(chats.userId, userId))
-    if (!_chats) {
-        return redirect('/')
-    }
-    // if (!_chats.find(chat => chat.id === parseInt(chatId))) {
-    //     return redirect('/')
-    // }
-    return (
-        <div className="flex max-h-screen overflow-scroll ">
-            <div className="flex w-full max-h-screen overflow-scroll">
-                <div className="flex-[1] max-w-xs">
-                    <ChatSidebar />
-                </div>
-                <div className="max-h-screen p-4 overflow-scroll flex-[5]">
+  const currentChat = _chats.find((chat) => chat.id === parseInt(chatId));
+  const isPro = await checkSubscription();
 
-                </div>
-                <div className="flex-[3] border-l-4 border-l-slate-200">
-
-                </div>
-
-            </div>
+  return (
+    <div className="flex max-h-screen overflow-scroll">
+      <div className="flex w-full max-h-screen overflow-scroll">
+        {/* chat sidebar */}
+        <div className="flex-[1] max-w-xs">
+          <ChatSideBar chats={_chats} chatId={parseInt(chatId)} isPro={isPro} />
         </div>
-    )
-}
+        {/* pdf viewer */}
+        <div className="max-h-screen p-4 oveflow-scroll flex-[5]">
+          <PDFViewer pdf_url={currentChat?.pdfUrl || ""} />
+        </div>
+        {/* chat component */}
+        <div className="flex-[3] border-l-4 border-l-slate-200">
+          <ChatComponent chatId={parseInt(chatId)} />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Chat
+export default ChatPage;
